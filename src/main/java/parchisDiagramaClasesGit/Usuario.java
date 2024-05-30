688fdc7c579828196a50ebdc324e58db66c17ae3
EvoltoZ <128405303+EvoltoZ@users.noreply.github.com>
2024-05-30 21:53:26 +0200

    Fase 7 - Clase usuario creada

---
+++ src/main/java/parchisDiagramaClasesGit/Usuario.java
@@ -0,0 +1,19 @@
+package parchisDiagramaClasesGit;
+
+public class Usuario {
+    private String username;
+    private String password;
+
+    public Usuario(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+}
